2f139a54071544e2fcf8f71d9d199574c7156829
yefweb <yeferson.dev@gmail.com>
2023-03-28 10:57:24 -0500

    part1 seccion c

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,67 @@
+import { useState } from "react"
+
+
+
 function Hello(props) {
+  const { name, age } = props
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - age
+  }
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
+
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
+function Display([counter]) {
+  return (
+    <div>
+      {counter}
+    </div>
+  )
+}
+
+function Button({ text, handleClick }) {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
       <h1>Grettinds</h1>
-      <Hello  name={"Yeferson"} age={25}/>
+      <Hello name={"Yeferson"} age={25} />
+
+      <Display counter={counter} />
+
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
+
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />
     </div>
   )
 }
