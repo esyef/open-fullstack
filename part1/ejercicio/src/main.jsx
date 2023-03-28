891aa2d45cd4b2b83f471e95714da96dd248c85d
yefweb <yeferson.dev@gmail.com>
2023-03-28 09:15:49 -0500

    step 1.5

--- part1/ejercicio/src/main.jsx
+++ part1/ejercicio/src/main.jsx
@@ -38,29 +38,31 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  const exercises = parts.map(part => part.exercises)
+  const exercises = course.parts.map(part => part.exercises)
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
       <Content
-        parts={parts}
+        parts={course.parts}
       />
 
       <Total
