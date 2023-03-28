b30a345c987c31a563ca22f9c80eda4b343e1472
yefweb <yeferson.dev@gmail.com>
2023-03-28 17:24:11 -0500

    step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,20 +10,55 @@ const Display = () => {
   )
 }
 
-const Button = ({ text }) => {
+const Button = ({ text, handleClick }) => {
   return (
-    <button>
+    <button onClick={handleClick}>
       {text}
     </button>
   )
 }
 
-const Statics = () => {
+
+const Statics = ({ feedback, count }) => {
+
+  if (count === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  const feedBackAverage = () => {
+    let average = 0
+    const totalScore = feedback.reduce((acc, currentValue) => acc +  currentValue.score, 0)
+    average = totalScore / feedback.length
+    return average
+  }
+
+  const porcentageGood = () => {
+    const totalScore = feedback.reduce((acc, currentValue) => acc +  currentValue.score, 0)
+    const totalGood = feedback.find(good => good.name === 'good').score
+    return ((totalGood / totalScore) * 100).toFixed(2)
+  }
+
+
   return (
     <footer>
       <h2>
         Statics
       </h2>
+
+      <ul>
+        {feedback.map(el => (
+          <li key={el.name}>
+            {el.name} : {el.score}
+          </li>
+        ))}
+      </ul>
+      <p>all: {count}</p>
+      <p>average: {feedBackAverage()}</p>
+      <p>positive: {porcentageGood()} %</p>
+
+
     </footer>
   )
 }
@@ -35,17 +70,29 @@ const App = () => {
     { name: 'neutral', score: 0 },
     { name: 'bad', score: 0 },
   ])
+  const [count, setCount] = useState(0)
 
+
+  const incrementFeedBack = (type) => {
+    setFeedback((prevFeddBack) => prevFeddBack.map(element => {
+      if (element.name === type) {
+        return { ...element, score: element.score + 1 }
+      }
+      return element
+    }))
+
+    setCount(count + 1)
+  }
 
   return (
     <>
       <Display />
       <main>
-        <Button text='Good' />
-        <Button text='Netural' />
-        <Button text='Bad' />
+        <Button text='Good' handleClick={() => incrementFeedBack('good')} />
+        <Button text='Netural' handleClick={() => incrementFeedBack('neutral')} />
+        <Button text='Bad' handleClick={() => incrementFeedBack('bad')} />
       </main>
-      <Statics />
+      <Statics feedback={feedback} count={count} />
     </>
   )
 }
