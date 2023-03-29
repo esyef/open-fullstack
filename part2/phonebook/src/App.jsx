651f8f0d5012e61a501169ead742487260cf4fc3
yefweb <yeferson.dev@gmail.com>
2023-03-29 17:26:17 -0500

    The phonebook step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,34 +2,53 @@ import { useState } from "react"
 
 const App = () => {
   const [ persons, setPersons ] = useState([ {
-    name: 'Arto Hellas'
+    name: 'Arto Hellas',
+    number: '333-444-1234'
   } ])
   const [ newName, setNewName ] = useState('')
+  const [ number, setNumber ] = useState('')
 
-  const addName = (eve) => {
+  const addNewPerson = (eve) => {
     eve.preventDefault()
-    if (newName.trim().length <= 3) return
+
+    if (newName.trim().length < 3) return alert(`Name not must be empty or less than 3 character`)
+
     const isNameExist = persons.map((person) => person.name).includes(newName)
+
     if (isNameExist) {
-      alert(` ${ newName } is already added to phonebook`)
+      return alert(`${ newName } is already added to phonebook`)
     }
+
     const newPerson = {
-      name: newName
+      name: newName,
+      number: number
     }
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNumber('')
   }
 
   const handleNameChange = (eve) => {
     setNewName(eve.target.value)
   }
 
+  const handleNumberChange = (eve) => {
+    const inputPhoneNumber = eve.target.value
+
+    // Regex: allow number phone start whith a number, and accept dashes and whitespace
+    const phoneNumberRegex = /^(?!-)[0-9- ]*$/
+
+    if (phoneNumberRegex.test(inputPhoneNumber)) {
+      setNumber(inputPhoneNumber)
+    }
+  }
+
   return (
     <div>
-      <h2>
+      <h1>
         Phonebook
-      </h2>
-      <form onSubmit={ addName }>
+      </h1>
+      <form onSubmit={ addNewPerson }>
         <div>
           <label htmlFor="name">
             name:
@@ -38,10 +57,30 @@ const App = () => {
             type="text"
             name="name"
             id='name'
-            placeholder="Write a new name"
+            placeholder="Jhon Doe"
             value={ newName }
             onChange={ handleNameChange }
+            required
           />
+        </div>
+        <div>
+          <label htmlFor="phone">
+            number:
+          </label>
+          <input
+            type='text'
+            placeholder='999-452-112-123'
+            name='phone'
+            id='phone'
+            onChange={ handleNumberChange }
+            value={ number }
+            required
+          />
+        </div>
+        <div>
+          <button type='submit'>
+            add
+          </button>
         </div>
       </form>
 
@@ -53,7 +92,7 @@ const App = () => {
         {
           persons.map((person) => (
             <li key={ person.name }>
-              { person.name }
+              <p> { person.name } : <span> { person.number } </span> </p>
             </li>
           ))
         }
