a7cb4cb98deba8329b4c6d6f2018f816de1dd245
yefweb <yeferson.dev@gmail.com>
2023-03-29 16:05:31 -0500

    The phonebook step1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react"
+
+const App = () => {
+  const [ persons, setPersons ] = useState([ {
+    name: 'Arto Hellas'
+  } ])
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (eve) => {
+    eve.preventDefault()
+    if (newName.trim().length <= 3) return
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (eve) => {
+    setNewName(eve.target.value)
+  }
+
+  return (
+    <div>
+      <h2>
+        Phonebook
+      </h2>
+      <form onSubmit={ addName }>
+        <div>
+          <label htmlFor="name">
+            name:
+          </label>
+          <input
+            type="text"
+            name="name"
+            id='name'
+            placeholder="Write a new name"
+            value={ newName }
+            onChange={ handleNameChange }
+          />
+        </div>
+      </form>
+
+      <h2>
+        Numbers
+      </h2>
+      <ul>
+
+        {
+          persons.map((person) => (
+            <li key={ person.name }>
+              { person.name }
+            </li>
+          ))
+        }
+
+      </ul>
+
+    </div>
+  )
+}
+
+export default App
