05f5c51c599ab854cfc67961fbcf7c762955303e
yefweb <yeferson.dev@gmail.com>
2023-03-30 16:47:37 -0500

    Data countries step 3, section part2-c ended

--- part2/dataCountries/src/components/CountryInfo.jsx
+++ part2/dataCountries/src/components/CountryInfo.jsx
@@ -1,6 +1,23 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const CountryInfo = (props) => {
+  const [ capitalInfo ] = useState(props.country.capital[ 0 ])
+  const [ weatherReport, setWeatherReport ] = useState({})
+
   const { name: { common }, capital, languages, flags: { png, alt }, population } = props.country
   const listLanguages = Object.values(languages)
+
+  const API_KEY = import.meta.env.VITE_WEATHER_STACK_API_KEY
+  const URL_BASE = `http://api.weatherstack.com/current?access_key=${ API_KEY }&query=${ capitalInfo }`
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(URL_BASE)
+      setWeatherReport(response.data.current)
+    }
+    fetchData()
+  }, [ capitalInfo ])
 
   return (
     <section>
@@ -28,6 +45,12 @@ const CountryInfo = (props) => {
       </ul>
 
       <img src={ png } alt={ alt } />
+
+      <article>
+        <p><strong>temperature: </strong> <span>{ weatherReport.temperature } celcius</span></p>
+        <img src={ weatherReport.weather_icons } alt='Weather icons' />
+        <p><strong>wind: </strong> <span>{ weatherReport.wind_speed } mph</span> <span>direction: { weatherReport.wind_dir }</span></p>
+      </article>
     </section>
   )
 }
